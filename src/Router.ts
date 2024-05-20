110b6c5e648dd417e208de847f7ba9291c6f95e3
sagun03 <sagunsaluja13@gmail.com>
2024-05-20 07:55:46 -0400

    updated the index.ts

--- src/Router.ts
+++ src/Router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import OrderRourtes from './routes/order.routes'
 import AuthRouters from './routes/auth.routes';
+import ProductRouters from './routes/product.routes'
 
 const router = Router();
 
 router.use('/auth', AuthRouters)
 router.use('/orders', OrderRourtes)
+router.use("/products", ProductRouters)
 
 export default router
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -5,27 +5,27 @@ const router = express.Router();
 const productController = ProductController.getInstance();
 
 // CREATE a new product
-router.post('/products', async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   await productController.createProduct(req, res);
 });
 
 // UPDATE an existing product
-router.put('/products/:id', async (req: Request, res: Response) => {
+router.put('/:id', async (req: Request, res: Response) => {
   await productController.updateProduct(req, res);
 });
 
 // DELETE a product
-router.delete('/products/:id', async (req: Request, res: Response) => {
+router.delete('/:id', async (req: Request, res: Response) => {
   await productController.deleteProduct(req, res);
 });
 
 // GET a product by ID
-router.get('/products/:id', async (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   await productController.getProductById(req, res);
 });
 
 // GET ALL products
-router.get('/products', async (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
   await productController.getAllProducts(req, res);
 });
 
