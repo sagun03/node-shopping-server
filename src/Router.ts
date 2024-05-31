0762706262809f0343a50c01b4b08c47049e43cc
Aseem4091 <https://github.com/sagun03/furnishFantasy-ui.git>
2024-05-31 01:13:49 -0400

    Inventory Management basic Api Implemenation

--- src/Router.ts
+++ src/Router.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import OrderRourtes from './routes/order.routes'
 import AuthRouters from './routes/auth.routes';
 import ProductRouters from './routes/product.routes'
+import InventoryRouters from './routes/inventory.routes'
 
 const router = Router();
 
 router.use('/auth', AuthRouters)
 router.use('/orders', OrderRourtes)
 router.use("/products", ProductRouters)
+router.use("/Inventory",InventoryRouters)
+
 
 export default router
