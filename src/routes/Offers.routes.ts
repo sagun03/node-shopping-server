f8230e458f69aa06b283a843a9f3bb735fd67d97
Aseem4091 <https://github.com/sagun03/furnishFantasy-ui.git>
2024-06-28 23:37:33 -0400

    I have written the swagger file for APIs of offer and inventory

--- src/routes/Offers.routes.ts
+++ src/routes/Offers.routes.ts
@@ -1,31 +1,174 @@
 import express, { Request, Response } from 'express';
 import OfferController from '../controllers/Inventory/Offer.controller';
-import { validateOfferData,validateOfferId } from '../middlewares/InventoryManagement/OfferMiddleware';
+import { validateOfferData, validateOfferId } from '../middlewares/InventoryManagement/OfferMiddleware';
 
 const router = express.Router();
 const offerController = OfferController.getInstance();
 
-// POST - CREATE a new offer
+/**
+ * @swagger
+ * tags:
+ *   name: Offers
+ *   description: Offer management API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     OfferDTO:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         description:
+ *           type: string
+ *         price:
+ *           type: number
+ *         available:
+ *           type: boolean
+ *     OfferInputDTO:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         description:
+ *           type: string
+ *         price:
+ *           type: number
+ *         available:
+ *           type: boolean
+ */
+
+/**
+ * @swagger
+ * /offers:
+ *   post:
+ *     summary: Create a new offer
+ *     tags: [Offers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/OfferInputDTO'
+ *     responses:
+ *       201:
+ *         description: Offer created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/OfferDTO'
+ *       400:
+ *         description: Invalid input
+ */
 router.post('/', validateOfferData(), async (req: Request, res: Response) => {
   await offerController.createOffer(req, res);
 });
 
-// PUT - UPDATE an existing offer by ID
+/**
+ * @swagger
+ * /offers/{id}:
+ *   put:
+ *     summary: Update an existing offer by ID
+ *     tags: [Offers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Offer ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/OfferInputDTO'
+ *     responses:
+ *       200:
+ *         description: Offer updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/OfferDTO'
+ *       400:
+ *         description: Invalid input
+ *       404:
+ *         description: Offer not found
+ */
 router.put('/:id', validateOfferId(), validateOfferData(), async (req: Request, res: Response) => {
   await offerController.updateOffer(req, res);
 });
 
-// DELETE - DELETE an offer by ID
+/**
+ * @swagger
+ * /offers/{id}:
+ *   delete:
+ *     summary: Delete an offer by ID
+ *     tags: [Offers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Offer ID
+ *     responses:
+ *       200:
+ *         description: Offer deleted successfully
+ *       404:
+ *         description: Offer not found
+ */
 router.delete('/:id', validateOfferId(), async (req: Request, res: Response) => {
   await offerController.deleteOffer(req, res);
 });
 
-// GET - GET an offer by ID
+/**
+ * @swagger
+ * /offers/{id}:
+ *   get:
+ *     summary: Get an offer by ID
+ *     tags: [Offers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Offer ID
+ *     responses:
+ *       200:
+ *         description: Offer details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/OfferDTO'
+ *       404:
+ *         description: Offer not found
+ */
 router.get('/:id', validateOfferId(), async (req: Request, res: Response) => {
   await offerController.getOfferById(req, res);
 });
 
-// GET - GET ALL offers
+/**
+ * @swagger
+ * /offers:
+ *   get:
+ *     summary: Get all offers
+ *     tags: [Offers]
+ *     responses:
+ *       200:
+ *         description: List of all offers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/OfferDTO'
+ */
 router.get('/', async (req: Request, res: Response) => {
   await offerController.getAllOffers(req, res);
 });
--- src/routes/inventory.routes.ts
+++ src/routes/inventory.routes.ts
@@ -1,32 +1,181 @@
 import express, { Request, Response } from 'express';
 import InventoryController from '../controllers/Inventory/inventory.controller';
-import { validateInventoryData,validateInventoryId } from '../middlewares/InventoryManagement/InventoryMiddleware';
+import { validateInventoryData, validateInventoryId } from '../middlewares/InventoryManagement/InventoryMiddleware';
 import { inventorySchema } from '../schemas/InventoryManagement/InventorySchema';
 
 const router = express.Router();
 const inventoryController = InventoryController.getInstance();
 
-// POST - CREATE a new inventory
+/**
+ * @swagger
+ * tags:
+ *   name: Inventory
+ *   description: Inventory Management API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     InventoryDTO:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         productID:
+ *           type: string
+ *         offerId:
+ *           type: string
+ *         quantity:
+ *           type: number
+ *         location:
+ *           type: string
+ *         status:
+ *           type: string
+ *           enum: [in stock, out of stock]
+ *     InventoryInputDTO:
+ *       type: object
+ *       properties:
+ *         productID:
+ *           type: string
+ *         offerId:
+ *           type: string
+ *         quantity:
+ *           type: number
+ *         location:
+ *           type: string
+ *         status:
+ *           type: string
+ *           enum: [in stock, out of stock]
+ */
+
+/**
+ * @swagger
+ * /inventory:
+ *   post:
+ *     summary: Create a new inventory
+ *     tags: [Inventory]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/InventoryInputDTO'
+ *     responses:
+ *       201:
+ *         description: Inventory created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/InventoryDTO'
+ *       400:
+ *         description: Invalid input
+ */
 router.post('/', validateInventoryData(), async (req: Request, res: Response) => {
   await inventoryController.createInventory(req, res);
 });
 
-// PUT - UPDATE an existing inventory by ID
+/**
+ * @swagger
+ * /inventory/{id}:
+ *   put:
+ *     summary: Update an existing inventory by ID
+ *     tags: [Inventory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Inventory ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/InventoryInputDTO'
+ *     responses:
+ *       200:
+ *         description: Inventory updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/InventoryDTO'
+ *       400:
+ *         description: Invalid input
+ *       404:
+ *         description: Inventory not found
+ */
 router.put('/:id', validateInventoryId(), validateInventoryData(), async (req: Request, res: Response) => {
   await inventoryController.updateInventory(req, res);
 });
 
-// DELETE - DELETE an inventory by ID
+/**
+ * @swagger
+ * /inventory/{id}:
+ *   delete:
+ *     summary: Delete an inventory by ID
+ *     tags: [Inventory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Inventory ID
+ *     responses:
+ *       200:
+ *         description: Inventory deleted successfully
+ *       404:
+ *         description: Inventory not found
+ */
 router.delete('/:id', validateInventoryId(), async (req: Request, res: Response) => {
   await inventoryController.deleteInventory(req, res);
 });
 
-// GET - GET an inventory by ID
+/**
+ * @swagger
+ * /inventory/{id}:
+ *   get:
+ *     summary: Get an inventory by ID
+ *     tags: [Inventory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Inventory ID
+ *     responses:
+ *       200:
+ *         description: Inventory details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/InventoryDTO'
+ *       404:
+ *         description: Inventory not found
+ */
 router.get('/:id', validateInventoryId(), async (req: Request, res: Response) => {
   await inventoryController.getInventoryById(req, res);
 });
 
-// GET - GET ALL inventories
+/**
+ * @swagger
+ * /inventory:
+ *   get:
+ *     summary: Get all inventories
+ *     tags: [Inventory]
+ *     responses:
+ *       200:
+ *         description: List of all inventories
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/InventoryDTO'
+ */
 router.get('/', async (req: Request, res: Response) => {
   await inventoryController.getAllInventories(req, res);
 });
