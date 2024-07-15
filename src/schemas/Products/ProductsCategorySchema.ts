e4cc4cf2d20c876f8cf75c487a5d2a8e1c0f0b95
Aseem4091 <https://github.com/sagun03/furnishFantasy-ui.git>
2024-07-14 20:55:01 -0400

    i have made changes to the blob storage part

---
+++ src/schemas/Products/ProductsCategorySchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const imageUploadSchema = z.object({
+  file: z
+    .instanceof(Buffer)
+    .refine((file) => file.byteLength > 0, {
+      message: "File must not be empty",
+    }),
+  mimetype: z.enum(['image/jpeg', 'image/png']),
+});
